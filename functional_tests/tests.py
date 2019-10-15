b971e6e0f222465812b58ce36d6b5d94047ce845
boberstarosta <boberstarosta@gmail.com>
2019-10-15 12:57:13 +0200

    integrated bootstrap for styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -13,5 +13,19 @@ class NewVisitorTest(LiveServerTestCase):
     def test_home_page_loads(self):
         # John goes to examinator home page
         self.browser.get(self.live_server_url)
-        # He can see Examinator in the title
+
+        # He can see Examinator in the title and header
         self.assertIn('Examinator', self.browser.title)
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('Examinator', header.text)
+
+        # There's a prominent button inviting him to create a new exam
+        self.browser.set_window_size(1024, 768)
+        button = self.browser.find_element_by_css_selector('button#create_exam')
+        self.assertIn('new exam', button.text.lower())
+        self.assertTrue(button.size['width'] > 200)
+        self.assertAlmostEqual(
+            button.location['x'] + button.size['width'] / 2,
+            512,
+            delta=10
+        )
