17291a4bd8dd089c53fc7bef98cdafa5008dd22e
boberstarosta <boberstarosta@gmail.com>
2019-10-15 16:08:01 +0200

    removed functional tests

--- exams/tests/tests.py
+++ exams/tests/tests.py
@@ -8,3 +8,10 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
+
+
+class NewExamTest(TestCase):
+
+    def test_uses_new_exam_template(self):
+        response = self.client.get('/new_exam')
+        self.assertTemplateUsed(response, 'new.html')
